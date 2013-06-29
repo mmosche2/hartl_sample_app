849f55ee49239b718867c89fe495fd9c6e3a4479
Mark <mark.moschel@gmail.com>
2013-06-29 13:00:03 -0500

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+  
   def home
+  	@title = 'ROR Sample | Home'
   end
 
   def help
+  	@title = 'ROR Sample | Help'
   end
+
+  def about
+  	@title = 'ROR Sample | About'
+	end
+
 end
