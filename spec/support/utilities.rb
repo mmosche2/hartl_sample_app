6b091c1b1cf220c61d95511b4c46d95504a1aea2
Mark <mark.moschel@gmail.com>
2013-07-01 10:30:49 -0500

    refactor static_pages spec and create application helper spec

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "ROR Sample"
-  if !page_title || page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
