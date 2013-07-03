ea82b50f05838940a1c259bd2990024c0101ce13
Mark <mark.moschel@gmail.com>
2013-07-03 09:41:56 -0500

    add user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,20 @@ class UsersController < ApplicationController
   	@title = @user.name
   end
 
+  def edit
+    @title = 'Edit user'
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = 'Profile updated'
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,7 +42,7 @@ describe "User pages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
       end
 
@@ -57,5 +57,41 @@ describe "User pages" do
   	it { should have_selector('h1',    text: user.name) }
   	it { should have_selector('title', text: user.name) }
   end	
+
+  describe 'edit' do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit edit_user_path(user) }
+
+  	describe 'page' do
+  		it { should have_selector('h1', text: 'Update your profile') }
+  		it { should have_link('change', href: 'http://gravatar.com/emails') }
+  		it { should have_selector('title', text: 'Edit user') }
+  	end
+
+  	describe 'with invalid info' do 
+  		before { click_button 'Save changes' }
+
+  		it { should have_content('error') }
+  	end
+
+  	describe 'with valid info' do
+  		let(:new_name) { "New Name" }
+  		let(:new_email) { "new@example.com" }
+  		before do
+  			fill_in "Name", with: new_name
+  			fill_in "Email", with: new_email
+  			fill_in "Password", with: user.password
+  			fill_in "Confirm Password", with: user.password
+  			click_button 'Save changes'
+  		end
+
+  		it { should have_success_message }
+  		it { should have_selector('title', text: new_name) }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
+  	end
+
+  end
 
 end
