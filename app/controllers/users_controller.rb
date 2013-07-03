c4c41b4d310b96bbc138f1b510dcce4542e23ef4
Mark <mark.moschel@gmail.com>
2013-07-03 10:58:55 -0500

    users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user,   only: [:edit, :update]
   
+  def index
+    @title = 'All users'
+    @users = User.all    
+  end
+
   def new
   	@title = 'Sign up'
   	@user = User.new
@@ -23,11 +30,9 @@ class UsersController < ApplicationController
 
   def edit
     @title = 'Edit user'
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = 'Profile updated'
       sign_in @user
@@ -36,5 +41,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in"
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
