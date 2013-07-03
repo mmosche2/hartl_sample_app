4aebe569288339559d024fa4f7ed077ea4b918c2
Mark <mark.moschel@gmail.com>
2013-07-03 01:19:40 -0500

    add sign out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			
-			flash[:success] = "Signed in!"
+			sign_in user
 			redirect_to user
 		else
 			flash.now[:error] = "Invalid email/password combination"
@@ -17,6 +16,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		sign_out
+		redirect_to root_url
 	end
 
 end
